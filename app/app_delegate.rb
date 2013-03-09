498c18f2bd7caec2b04089867af311518c9cb8bc
Darrin Holst <darrinholst@gmail.com>
2013-03-08 20:10:22 -0600

    set the title bar back to black

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,11 +1,12 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.makeKeyAndVisible
+    UIApplication.sharedApplication.setStatusBarStyle(UIStatusBarStyleBlackOpaque)
 
     slidingController = ECSlidingViewController.alloc.initWithNibName(nil, bundle: nil)
     slidingController.topViewController = RootController.alloc.initWithNibName(nil, bundle: nil)
 
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
     @window.rootViewController = slidingController
 
     true
